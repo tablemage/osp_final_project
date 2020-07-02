299210e2e2708a37452d5f83e668e5278a222634
tablemage <jhp_star@naver.com>
2020-07-03 02:36:44 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+pip install curl
+pip install werkzeug
+pip install numpy
+pip install flask_restful
+pip install nltk
+pip install argparse
+pip install json
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee/etc/apt/sources.list.d/yarn.list
+
+sudo apt update 
+sudo apt install yarn
+python3 api.py&yarn serve
