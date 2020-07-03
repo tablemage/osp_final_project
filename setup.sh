8bf4e14a5b35dc2cb6f9825d7139c4da79b67c98
tablemage <jhp_star@naver.com>
2020-07-03 12:18:28 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,12 @@ pip install flask_restful
 pip install nltk
 pip install argparse
 pip install json
+pip install requests
+pip install timeit
+pip install math
+pip install re
+pip install bs4
+pip install flask
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee/etc/apt/sources.list.d/yarn.list
 
